602facd787377fba5c92f16565aac9c8092216e3
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-02-09 17:32:26 -0600

    Update arguments.py
    added new arguments

--- src/arguments.py
+++ src/arguments.py
@@ -12,6 +12,20 @@ class DateAction(argparse.Action):
         date = datetime.strptime(values, r'%Y-%m-%d')
         setattr(args, self.dest, date)
 
+class LoadFileAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        file_location = values
+        if file_location:
+            try:
+                with open(file_location, 'r') as f:
+                    lines = [line.strip() for line in f if not line.startswith('#')]
+                    setattr(namespace, self.dest, lines)
+            except FileNotFoundError:
+                print(f"Warning: Could not find file at location '{file_location}'")
+                setattr(namespace, self.dest, [])
+        else:
+            setattr(namespace, self.dest, [])
+
 def extract_channel_ids(channel_ids):
     pattern = r"(\d+)|(https://discord.com/channels/[^/]+/(\d+))"
     results = []
@@ -19,6 +33,8 @@ def extract_channel_ids(channel_ids):
         match = re.search(pattern, channel_id)
         if match:
             results.append(match.group(1) if match.group(1) else match.group(3))
+        else:
+            print(f'Warning could not find discord channel id in: {channel_id}')
     return results
 
 def get_args():
@@ -36,7 +52,16 @@ def get_args():
         'channel_ids',
         type=str,
         nargs='*',
-        help='channel id or channel url'
+        help='channel id or channel url',
+        default=[]
+    )
+
+    parser.add_argument(
+        '--file',
+        type=str,
+        help='File containing channel ids to download, one channel id per line. Lines starting with "#" are considered as comments and ignored',
+        action=LoadFileAction,
+        default=None
     )
 
     parser.add_argument(
@@ -57,15 +82,29 @@ def get_args():
         '--dm-format',
         type=str,
         help='The format that attachments from direct messages will be downloaded with',
-        default='direct_messages/{username}_{user_id}/{date:%Y-%m-%d}_{id}_{filename}.{ext}'
+        default='direct_messages/{username}_{channel_id}/{username}_{user_id}/{date:%Y-%m-%d}_{id}_{filename}.{ext}'
     )
 
-    # parser.add_argument(
-    #     '--sleep',
-    #     type=int,
-    #     help='How long to sleep before attempting to download the next attachment',
-    #     default=0
-    # )
+    parser.add_argument(
+        '--restrict-filenames',
+        type=bool,
+        help='Restrict filenames to only ASCII characters and remove spaces',
+        default=False
+    )
+
+    parser.add_argument(
+        '--windows-filenames',
+        type=bool,
+        help='Force filenames to be Windows-compatible, filenames are Windows-compatible when using Windows',
+        default=False
+    )
+
+    parser.add_argument(
+        '--sleep',
+        type=int,
+        help='How long to sleep before attempting to download the next attachment, Default is 0',
+        default=0
+    )
 
     parser.add_argument(
         '--max-retries',
@@ -75,20 +114,27 @@ def get_args():
     )
 
     parser.add_argument(
-        '--filter-user-id',
+        '--filter-by-user-id',
         type=str,
         help='Only download attachments posted by this user id(s)',
         action=ListAction,
         default=[]
     )
 
-    # parser.add_argument(
-    #     '--filter-username',
-    #     type=str,
-    #     help='Only download attachments posted by this username (Usernames are not unique and do not contain the #0000)',
-    #     action=ListAction,
-    #     default=[]
-    # )
+    parser.add_argument(
+        '--filter-by-username',
+        type=str,
+        help='Only download attachments posted by this username(s) (Usernames are not unique! Usernames do not contain the #0000)',
+        action=ListAction,
+        default=[]
+    )
+
+    parser.add_argument(
+        '--message-count',
+        type=int,
+        help='Only download attachments from the last # messages',
+        default=-1
+    )
 
     # parser.add_argument(
     #     '--filter-date',
@@ -113,5 +159,7 @@ def get_args():
 
     # Parse the arguments passed in the command-line
     args = parser.parse_args()
+    args.channel_ids += args.file if args.file is not None else []
     args.channel_ids = extract_channel_ids(args.channel_ids)
+    print(args.channel_ids)
     return args
