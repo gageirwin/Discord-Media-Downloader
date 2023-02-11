fb2a5280f0c6a7f366d3b66d12a04455f3796c2c
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-02-10 23:43:13 -0600

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -3,7 +3,11 @@ import requests
 import os
 import time
 import re
+from datetime import datetime
 from src.logger import logger
+
+def convert_discord_timestamp(timestamp):
+    return datetime.strptime(timestamp, r"%Y-%m-%dT%H:%M:%S.%f%z")
 
 def calculate_md5(file_path) -> str:
     hash_md5 = hashlib.md5()
@@ -39,82 +43,73 @@ def extract_channel_ids(channel_ids):
             logger.warning(f'Could not find discord channel id in: {channel_id}')
     return results
 
-def download(url:str, filepath:str) -> None:
+def download(url:str, filepath:str, simulate=False) -> None:
     file_path, filename = os.path.split(filepath)
     logger.info(f"Downloading: {filename}")
     logger.debug(f"Path: {file_path}")
     logger.debug(f"URL: {url}")
 
-    local_md5 = calculate_md5(filepath) if os.path.exists(filepath) else None
-    with requests.get(url, stream=True) as r:
-        if r.status_code != 200:
-            return r.status_code
-        server_md5 = r.headers.get('ETag', '')
-        if not server_md5:
-            logger.warning("No server hash found for attachment")
-        if server_md5 == f'"{local_md5}"':
-            return 1
-        total = int(r.headers.get('content-length', 0))
-        if not os.path.exists(file_path):
-            logger.debug("Creating Path because it did not exist")
-            os.makedirs(file_path)
-        with open(filepath, 'wb') as f:
+    if not simulate:
+        local_md5 = calculate_md5(filepath) if os.path.exists(filepath) else None
+        with requests.get(url, stream=True) as r:
+            if r.status_code != 200:
+                return r.status_code
+            server_md5 = r.headers.get('ETag', '')
+            if not server_md5:
+                logger.warning("No server hash found for attachment")
+            if server_md5 == f'"{local_md5}"':
+                return 1
+            total = int(r.headers.get('content-length', 0))
+            if not os.path.exists(file_path):
+                logger.debug("Creating Path because it did not exist")
+                os.makedirs(file_path)
             start = time.time()
-            bar_len = 0
-            downloaded = 0
-            for chunk in r.iter_content(chunk_size=8192):
-                downloaded += len(chunk) 
-                f.write(chunk)
-                bar_len = print_download_bar(total, downloaded, start, bar_len)
-        print()
+            with open(filepath, 'wb') as f:
+                bar_len = 0
+                downloaded = 0
+                for chunk in r.iter_content(chunk_size=8192):
+                    downloaded += len(chunk) 
+                    f.write(chunk)
+                    bar_len = print_download_bar(total, downloaded, start, bar_len)
+            print()
         return r.status_code
+    start = time.time()
+    print_download_bar(1, 1, start, 0)
+    print()
+    return 200
+
+def bytes_to_string(bytes:str):
+    if bytes/2**10 < 100:
+        return (round(bytes/2**10, 1), 'KB')
+    if bytes/2**20 < 100:
+        return (round(bytes/2**20, 1), 'MB')
+    if bytes/2**30 < 100:
+        return (round(bytes/2**30, 1), 'GB')
+    if bytes/2**40 < 100:
+        return (round(bytes/2**40, 1), 'TB')    
 
 def print_download_bar(total, downloaded, start, bar_len) -> None:
     td = time.time() - start
-    
-    if td == 0:
-        rate = 0
-        eta = "00:00:00"
-    else:
-        rate = (downloaded)/td
-        eta = time.strftime("%H:%M:%S", time.gmtime((total-downloaded) / rate))
-
-    if rate/2**10 < 100:
-        rate = (round(rate/2**10, 1), 'KB')
-    elif rate/2**20 < 100:
-        rate = (round(rate/2**20, 1), 'MB')
-    else:
-        rate = (round(rate/2**30, 1), 'GB')
+    td = 0.0000001 if td == 0 else td
+    rate, rate_size = bytes_to_string((downloaded)/td)
+    eta = time.strftime("%H:%M:%S", time.gmtime((total-downloaded) / rate))
 
     if total:
         done = int(50*downloaded/total)
         bar_fill = '='*done
         bar_empty = ' '*(50-done)
-        if total/2**10 < 100:
-            total = (round(total/2**10, 1), 'KB')
-            downloaded = round(downloaded/2**10,1)
-        elif total/2**20 < 100:
-            total = (round(total/2**20, 1), 'MB')
-            downloaded = round(downloaded/2**20,1)
-        else:
-            total = (round(total/2**30, 1), 'GB')
-            downloaded = round(downloaded/2**30,1)
+        total, size = bytes_to_string(total)
+        downloaded, _ = bytes_to_string(downloaded)
     else:
         done = 50
         bar_fill = '?'*done
         bar_empty = ' '*(50-done)
-        if downloaded/2**10 < 100:
-            total = ('???', 'KB')
-            downloaded = round(downloaded/2**10,1)
-        elif downloaded/2**20 < 100:
-            total = ('???', 'MB')
-            downloaded = round(downloaded/2**20,1)
-        else:
-            total = ('???', 'GB')
-            downloaded = round(downloaded/2**30,1)
+        total = '???'
+        downloaded, size = bytes_to_string(downloaded)
+        eta = '??:??:??'
 
-    progress_bar = f'[{bar_fill}{bar_empty}] {downloaded}/{total[0]} {total[1]} at {rate[0]} {rate[1]}/s ETA {eta}'
+    progress_bar = f'[{bar_fill}{bar_empty}] {downloaded}/{total} {size} at {rate} {rate_size}/s ETA {eta}'
     overlap = bar_len - len(progress_bar)
     overlap_buffer = ' '*overlap if overlap > 0 else ''
     print(f'{progress_bar}{overlap_buffer}', end='\r')
-    return len(progress_bar) 
+    return len(progress_bar)
