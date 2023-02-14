af98704a84ccea71294cd084a436640179939e59
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-02-14 01:10:33 -0600

    Update __main__.py

--- discord_dl/__main__.py
+++ discord_dl/__main__.py
@@ -1,6 +1,6 @@
 from src import DiscordDownloader, get_args
 
 if __name__ == '__main__':
-    args = get_args()
-    dd = DiscordDownloader(args)
+    options = vars(get_args())
+    dd = DiscordDownloader(options)
     dd.run()
