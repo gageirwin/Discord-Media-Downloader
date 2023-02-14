d21516b9fdd92092bc71bd3e02107af525ce55ad
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-02-14 01:11:04 -0600

    Update discord_dl.py
    pass options dict instead of argparse object

--- discord_dl/src/discord_dl.py
+++ discord_dl/src/discord_dl.py
@@ -6,16 +6,39 @@ from src.logger import logger
 
 class DiscordDownloader():
 
-    def __init__(self, args) -> None:
-        self.args = args
-        self.args.channel_ids = extract_channel_ids(args.channel_ids)
-        for attr in vars(args):
-            if attr == 'token':
-                continue
-            logger.debug(f"{attr}: {getattr(args, attr)}")
-        if not os.path.exists(self.args.path):
-            logger.error(f"--path does not exist: {self.args.path}")
-            exit()
+    def __init__(self, options:dict) -> None:
+        self.token = options.get('token', None)
+        self.path = options.get('path', os.getcwd())
+        self.file = options.get('file', None)
+        self.channel_ids = options.get('channel_ids', [])
+        self.message_count = options.get('message_count', -1)
+        self.sleep = options.get('sleep', 0)
+        self.sleep_random = options.get('sleep_random', [0,0])
+        self.max_retries = options.get('max_retries', 10)
+        self.date = options.get('date', None)
+        self.date_before = options.get('date_before', None)
+        self.date_after = options.get('date_after', None)
+        self.username = options.get('username', [])
+        self.user_id = options.get('user_id', [])
+        self.channel_format = options.get('channel_format', 'downloads/{date:%Y-%m-%d}_{id}_{filename}.{ext}')
+        self.dm_format = options.get('dm_format', 'downloads/{date:%Y-%m-%d}_{id}_{filename}.{ext}')
+        self.windows_filenames = options.get('windows_filenames', False)
+        self.restrict_filenames = options.get('restrict_filenames', False)
+        self.simulate = options.get('simulate', False)
+
+        if self.token == None:
+            raise (f"No discord auth token passed")
+
+        for key, value in options.items():
+            if key == 'token':
+                logger.debug(f"{key}: '**********'")
+            else:
+                logger.debug(f"{key}: {value}")
+
+        if not os.path.exists(self.path):
+            raise (f"Download path does not exist: {self.path}")
+        
+        self.channel_ids = extract_channel_ids(self.channel_ids)
 
     def get_server_info(self, session, guild_id:str) -> dict:
         logger.info(f"Getting server info for server id {guild_id}")
@@ -46,13 +69,13 @@ class DiscordDownloader():
             messages_chunk = self.retrieve_messages(session, channel_id, before_message_id=last_message_id)
             messages += messages_chunk
             last_message_id = messages_chunk[-1]['id']
-            if self.args.message_count >= 0 and len(messages) >= self.args.message_count:
-                logger.debug(f"Got {len(messages[:self.args.message_count])} messages for channel id {channel_id}")
-                return self.find_messages(messages[:self.args.message_count])
+            if self.message_count >= 0 and len(messages) >= self.message_count:
+                logger.debug(f"Got {len(messages[:self.message_count])} messages for channel id {channel_id}")
+                return self.find_messages(messages[:self.message_count])
             if len(messages_chunk) < 50:
                 logger.debug(f"Got {len(messages)} messages for channel id {channel_id}")
                 return self.find_messages(messages)
-            mysleep(self.args.sleep, self.args.sleep_random)
+            mysleep(self.sleep, self.sleep_random)
 
     def retrieve_messages(self, session, channel_id:str, before_message_id:str=None) -> list:
         params = {'limit':50}
@@ -62,7 +85,7 @@ class DiscordDownloader():
         else:
             logger.info(f"Getting messages for channel id {channel_id}")
         retries = 0
-        while retries < self.args.max_retries:
+        while retries < self.max_retries:
             try:
                 messages = session.get(f'https://discord.com/api/v9/channels/{channel_id}/messages', params=params).json()
             except ConnectionError:
@@ -71,7 +94,7 @@ class DiscordDownloader():
                 logger.warning(f"{messages.status_code} Failed to get messages with url: {messages.url}")   
                 logger.info(f"Sleeping for {sleep} seconds")
                 time.sleep(sleep)
-                logger.info(f"Retrying download {retries}/{self.args.max_retries}")
+                logger.info(f"Retrying download {retries}/{self.max_retries}")
             else:
                 return messages
 
@@ -79,29 +102,29 @@ class DiscordDownloader():
         filtered_data = []
         for message in messages:
             message_date = convert_discord_timestamp(message['timestamp']).replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
-            if self.args.date and message_date != self.args.date:
-                logger.debug(f"Message date {message_date:%Y-%m-%d} != args.date {self.args.date:%Y-%m-%d} for message id {message['id']}")
+            if self.date and message_date != self.date:
+                logger.debug(f"Message date {message_date:%Y-%m-%d} != args.date {self.date:%Y-%m-%d} for message id {message['id']}")
                 continue
-            if self.args.date_before and message_date >= self.args.date_before:
-                logger.debug(f"Message date {message_date:%Y-%m-%d} >= args.date_before {self.args.date_before:%Y-%m-%d} for message id {message['id']}")
+            if self.date_before and message_date >= self.date_before:
+                logger.debug(f"Message date {message_date:%Y-%m-%d} >= args.date_before {self.date_before:%Y-%m-%d} for message id {message['id']}")
                 continue
-            if self.args.date_after and message_date <= self.args.date_after:
-                logger.debug(f"Message date {message_date:%Y-%m-%d} <= args.date_after {self.args.date_after:%Y-%m-%d} for message id {message['id']}")
+            if self.date_after and message_date <= self.date_after:
+                logger.debug(f"Message date {message_date:%Y-%m-%d} <= args.date_after {self.date_after:%Y-%m-%d} for message id {message['id']}")
                 continue
-            if self.args.username and message['author']['username'] not in self.args.username:
-                logger.debug(f"Message username {message['author']['username']} is not in args.username {self.args.username} for message id {message['id']}")
+            if self.username and message['author']['username'] not in self.username:
+                logger.debug(f"Message username {message['author']['username']} is not in args.username {self.username} for message id {message['id']}")
                 continue
-            if self.args.user_id and message['author']['id'] not in self.args.user_id:
-                logger.debug(f"Message user id {message['author']['id']} is not in args.user_id {self.args.user_id} for message id {message['id']}")
+            if self.user_id and message['author']['id'] not in self.user_id:
+                logger.debug(f"Message user id {message['author']['id']} is not in args.user_id {self.user_id} for message id {message['id']}")
                 continue
             filtered_data.append(message)
         return filtered_data
 
     def download_attachment(self, attachment:dict, variables:dict) -> None:
-        filepath = create_filepath(variables, self.args.path, self.args.channel_format, self.args.dm_format, self.args.windows_filenames, self.args.restrict_filenames)
+        filepath = create_filepath(variables, self.path, self.channel_format, self.dm_format, self.windows_filenames, self.restrict_filenames)
         retries = 0
-        while retries < self.args.max_retries:
-            result = download(attachment['url'], filepath, self.args.simulate)
+        while retries < self.max_retries:
+            result = download(attachment['url'], filepath, self.simulate)
             if result == 1:
                 logger.info('File already downloaded with matching hash and file name')
                 break
@@ -114,22 +137,22 @@ class DiscordDownloader():
                 logger.warning(f"{result} Failed to download url: {attachment['url']}")   
                 logger.info(f"Sleeping for {sleep} seconds")
                 time.sleep(sleep)
-                logger.info(f"Retrying download {retries}/{self.args.max_retries}")
+                logger.info(f"Retrying download {retries}/{self.max_retries}")
             else:
                 break
 
     def run(self):
-        headers = {'Authorization': self.args.token}
+        headers = {'Authorization': self.token}
         session = requests.Session()
         session.headers.update(headers)
-        for channel_id in self.args.channel_ids:
+        for channel_id in self.channel_ids:
             channel_messages = self.get_all_messages(session, channel_id)
-            variables = self.get_channel_info(session, channel_id)
+            channel_variables = self.get_channel_info(session, channel_id)
             for message in channel_messages:
                 for attachment in message['attachments']:
                     if 'https://cdn.discordapp.com' == attachment['url'][:27]:
                         logger.warning(f"Attachment not hosted by discord {attachment['url']}")
                         continue
-                    variables = {**create_format_variables(message, attachment), **variables}
+                    variables = {**create_format_variables(message, attachment), **channel_variables}
                     self.download_attachment(attachment, variables)
-                    mysleep(self.args.sleep, self.args.sleep_random)
+                    mysleep(self.sleep, self.sleep_random)
